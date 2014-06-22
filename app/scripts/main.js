4ef939ebbb310b4c54884edcdfa87d5275a57293
Thomas bredin-Grey <tbredin@gmail.com>
2014-06-22 23:26:47 +1000

    Update main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,1 @@
-$(function() {
-    var container = $('.card-bucket');
-    var navToggle = $('.nav__toggle')
-    var navMenu = $('.nav__menu')
-    // initialize
-    container.masonry({
-      columnWidth: '.card',
-      itemSelector: '.card',
-      gutter: '.card-spacer',
-      transitionDuration: 0
-    });
-
-    // container.masonry(); //seems to fix mobile broken gutter widths on init
-
-    navToggle.on('click', function(e) {
-        navMenu.slideToggle();
-    });
-});
+ 
